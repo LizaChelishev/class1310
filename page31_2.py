7c11fc2a76cbf9b825a4bcf8e3cf56ce8cc28e5e
LizaChelishev <90146659+LizaChelishev@users.noreply.github.com>
2021-10-13 20:16:11 +0300

    Update page31_2.py

--- page31_2.py
+++ page31_2.py
@@ -7,3 +7,9 @@ else:
 for index in range (number1, number2 + gap, gap )
     print(index)
 
+number1 = int(input('Enter first number: '))
+number2 = int(input('Enter second number: '))
+gap = (number1 - number2) // abs(number1 - number2)
+for index in range (number1, number2 + gap, gap):
+    print(index)
+
