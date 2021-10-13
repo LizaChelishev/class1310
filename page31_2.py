eeac47ff51fbfdc3ffc61955a923072f15597eca
LizaChelishev <90146659+LizaChelishev@users.noreply.github.com>
2021-10-13 19:28:27 +0300

    Create page31_2.py

---
+++ page31_2.py
@@ -0,0 +1,9 @@
+number1 = int(input('Enter first number: '))
+number2 = int(input('Enter second number: '))
+if number1 > number2:
+    gap = -1
+else: 
+    gap = 1 
+for index in range (number1, number2 + gap, gap )
+    print(index)
+
