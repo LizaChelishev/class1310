e8e876b3a0314147f33a9ca49d7da31a4a39dfd3
LizaChelishev <90146659+LizaChelishev@users.noreply.github.com>
2021-10-13 20:39:14 +0300

    Update page31_4.py

--- page31_4.py
+++ page31_4.py
@@ -1,7 +1,7 @@
 n = int(input('Enter a number: '))
 sum_divided_by3 = 0
-for n in range (3, n + 1, 3):
-    sum_divided_by3 += n
+for i in range (3, n + 1, 3):
+    sum_divided_by3 += i
 print(sum_divided_by3)
 
 
